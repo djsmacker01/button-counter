bfd581074d6e998113632a8f93781b6dda751ff1
Success Adedeji Adewale Nurudeen <djsmacker01@gmail.com>
2021-02-12 17:46:05 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
 
   const [counter, setCounter] = useState(1);
+
+  const clickEvent = () => setCounter(counter * 2)
   return (
     <div className="App">
       <button
         className='btn'
-        onClick={() => setCounter(counter*2)}>{counter} 
+        onClick={clickEvent}>{counter} 
       </button>
       <h2>click me</h2>
     </div>
